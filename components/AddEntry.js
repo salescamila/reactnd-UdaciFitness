fc8c72fa3add9e5dc47006078808e6ac40b0620e
Camila <salescamila1@gmail.com>
2019-06-06 00:12:57 -0400

    Changes for Api

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -61,7 +61,7 @@ export default class AddEntry extends Component {
 
     // Navigate to home
 
-    // Save to "DB"
+    submitEntry({ key, entry })
 
     // Clear local notification
   }
@@ -72,7 +72,7 @@ export default class AddEntry extends Component {
 
     // Route to Home
 
-    // Update "DB"
+    removeEntry(key)
   }
   render() {
     const metaInfo = getMetricMetaInfo()
